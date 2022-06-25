0fc742c60523fd400765d4cd7b449a751eeddf24
Aleksandrov-AA <sana115@mail.ru>
2022-06-25 14:00:50 +0300

    feat: function toReadable implementation (CodeWars for RS School)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function toReadable (number) {
-  
+    let string = number.toString(),
+    units, tens, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+    
+    if (String(number).length > 3) {
+        return console.log("I don't know this number. Я умею считать от 0 до 999");
+    }
+    
+    if (parseInt(number) === 0) {
+        return 'zero';
+    }
+
+    units = [ '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+    end = start;
+    chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+    }
+
+    chunksLen = chunks.length;
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+        chunk = parseInt(chunks[i]);
+        if (chunk) {
+            ints = chunks[i].split('').reverse().map(parseFloat);
+            if (ints[1] === 1) {
+                ints[0] += 10;
+            }
+            if ((word = units[ints[0]])) {
+                words.push(word);
+            }
+            if ((word = tens[ints[1]])) {
+                words.push(word);
+            }
+            if ((word = units[ints[2]])) {
+                words.push(word + ' hundred');
+            }
+        }
+    }
+
+    return words.reverse().join(' ');
 }
